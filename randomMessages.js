391d453bef4410037b042ac29c2e71115a2f7a66
Jboski <Jboski@Jaylens-MB-Pro.local>
2022-02-07 14:14:47 -0800

    Added messages and function

--- randomMessages.js
+++ randomMessages.js
@@ -1,12 +1,16 @@
 // This file creates random prompts when the function is run
 
 const messages = [
-    "Message 1",
-    "Message 2",
-   
-    
+    "We will not be remembered by our words, but by our kind deeds. Author Unknown",
+    "The only thing that can grow is the thing you give energy to. Ralph Waldo Emerson",
+    "Be positive! Negativity doesn't get you anywhere. Peter W. Murphy",   
+    "What have you done to inspire or help others? Stephanie Carroll, My Conversations with God Book 4",
+    "No greater gift there is, than a generous heart. Yoda",
+    "I enjoy every day, because I choose to. Tony Clark",
+    "The doors of wisdom are never shut. Benjamin Franklin"
 ]
 
-function generateMessage(array) {
-    console.log('Hey this JS file works.');
+function generateMessage(messages) {
+    let num = Math.floor(Math.random() * messages.length);
+    console.log(messages[num]);
 }
