f948c5c7860f41cbc087084e5158c5486f2787d8
Jboski <Jboski@Jaylens-MB-Pro.local>
2022-02-07 13:49:01 -0800

    added js file

---
+++ randomMessages.js
@@ -0,0 +1,12 @@
+// This file creates random prompts when the function is run
+
+const messages = [
+    "Message 1",
+    "Message 2",
+   
+    
+]
+
+function generateMessage(array) {
+    console.log('Hey this JS file works.');
+}
